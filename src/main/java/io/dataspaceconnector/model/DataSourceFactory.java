8ff9852eb23a87d4c5759724e1ef115c71e97bb4
Gökhan Kahriman <goekhan.kahriman@isst.fraunhofer.de>
2021-05-19 10:32:21 +0200

    chore: added DataSourceFactory

--- src/main/java/io/dataspaceconnector/model/DataSourceFactory.java
+++ src/main/java/io/dataspaceconnector/model/DataSourceFactory.java
@@ -8,11 +8,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataSourceFactory implements AbstractFactory<DataSource, DataSourceDesc> {
 
+    private static final String DEFAULT_PATH = "default";
+
+    /**
+     * @param desc The description of the entity.
+     * @return The new data source factory.
+     */
     @Override
     public DataSource create(DataSourceDesc desc) {
         return null;
     }
 
+    /**
+     * @param dataSource The data source entity.
+     * @param desc       The description of the new entity.
+     * @return True, if data source is updated.
+     */
     @Override
     public boolean update(DataSource entity, DataSourceDesc desc) {
         return false;
